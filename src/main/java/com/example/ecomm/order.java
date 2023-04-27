551a92fd19c018c021f40bd9eaed62d644ccf638
AnujShakya123 <130181154+AnujShakya123@users.noreply.github.com>
2023-04-27 18:53:21 +0530

    #java #ecommerce Website

---
+++ src/main/java/com/example/ecomm/order.java
@@ -0,0 +1,25 @@
+package com.example.ecomm;
+
+import javafx.collections.ObservableList;
+
+public class order {
+    public static boolean placeOrder(Customer customer,Product product){
+        try{
+            String placeOrder="INSERT INTO orderrrrr(Customer_id,product_id,status)VALUES(" + customer.getId() +","+ product.getId()+",'Ordered')";
+            DatabaseConnection dbConn=new DatabaseConnection();
+            return dbConn.insertUpdate(placeOrder);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static int placeOrderMultipleProducts(ObservableList<Product> productObservableList,Customer customer){
+        int count=0;
+        for(Product product:productObservableList){
+            if(placeOrder(customer,product))
+                count++;
+        }
+        return count;
+    }
+}
